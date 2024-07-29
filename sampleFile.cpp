86f4689c0d07e9bdc2c0a0111a40b9455318b478
askamble7771 <askamble7771@gmail.com>
2024-07-29 17:04:21 +0530

    Added intial commit

---
+++ sampleFile.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // This is a preprocessor directive that includes the input-output stream library
+
+int main() {
+    // This is the main function, the entry point of any C++ program
+
+    std::cout << "Hello, World!" << std::endl; // This line prints "Hello, World!" to the console
+
+    return 0; // This line indicates that the program ended successfully
+}
